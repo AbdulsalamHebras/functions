3ce341a8b23216ec5acdd96a52b8ba58b30b1b8c
AbdulsalamHebras <salamheras@gmail.com>
2024-10-01 15:12:01 -0700

    frist commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <h1>add methos -{{ add(5,3,20) }}</h1>
+<h1>muilply methos -{{ muitply(45) }}</h1> -->
+  <h1>{{ name }}</h1>
+  <button @click="changeName">change name</button>
+  <h1>{{ counter }}</h1>
+  <button @click="increment(5, $event)">increase</button>
+  <button @click="decrement(5)">decrease</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      // basenum:6
+      name: "salam",
+      counter: 0,
+    };
+  },
+  methods: {
+    //   add(x,y,z){
+    //     return x+y+z;
+    //   },
+    //   muitply(num){
+    // return  num*this.basenum
+    //   }
+    changeName() {
+      return (this.name = "Hebras");
+    },
+    increment(num, event) {
+      this.counter += num
+      console.log(event)
+    },
+    decrement(num) {
+      this.counter -= num;
+    },
+  },
+};
 </script>
 
 <style>
